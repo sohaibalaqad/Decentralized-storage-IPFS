76644f903f54733d07de00abaeb638a5eef03614
Sohaib Alaqad <sohaibalaqad@gmail.com>
2023-07-05 18:03:45 +0300

    Create nodes files [model, migration, controller]

--- database/migrations/2023_07_04_234259_create_partners_table.php
+++ database/migrations/2023_07_04_234259_create_partners_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('partners', function (Blueprint $table) {
             $table->id();
-            $table->string('neme');
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('Country')->nullable();
