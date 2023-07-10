62c282243cbdb27c2b0cafddb10c45f52825e051
Sohaib Alaqad <sohaibalaqad@gmail.com>
2023-07-10 17:53:07 +0300

    edit .env.example file

--- app/Models/Node.php
+++ app/Models/Node.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Node extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'ip',
+        'status',
+        'partner_id',
+        'disk_size',
+        'used_disk_size',
+        'free_disk_size'
+    ];
 }
